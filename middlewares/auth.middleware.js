810e6090d008c88819964aa1559901bbb6ba0663
Shripad Dhanshetti <shripad1483@gmail.com>
2026-01-30 21:22:13 +0530

    feat(url.routes.js): Write /shorten endpoint to generate short codes for urls.

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -15,6 +15,16 @@ export function authenticationMiddleware(req, res, next){
 
     const payload = validateUserToken(token);
 
-    req.user() = payload;
+    req.user = payload;
+    next();
+}
+
+export function ensureAuthenticated(req, res, next){
+    if(!req.user || !req.user.id){ 
+       return res
+        .status(401)
+        .json({ error: "You must be logged in to access this resource" });
+    }
+
     next();
 }
---
+++ routes/url.routes.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+import { shortenPostRequestBodySchema } from "../validations/request.validation.js";
+
+import { db } from "../db/index.js";
+import { urlsTable } from "../models/url.model.js";
+
+import { ensureAuthenticated } from "../middlewares/auth.middleware.js"; 
+
+import { nanoid } from "nanoid";
+
+const router = express.Router();
+
+router.post("/shorten", ensureAuthenticated, async (req, res) => {
+  const userID = req.user?.id;
+   
+  const validationResult = await shortenPostRequestBodySchema.safeParseAsync(
+    req.body,
+  );
+
+  if (validationResult.error) {
+    return res.status(400).json({ error: validationResult.error.format() });
+  }
+
+  const { url, code } = validationResult.data;
+  const shortCode = code ?? nanoid(6);
+
+  const [result] = await db
+    .insert(urlsTable)
+    .values({
+      shortCode,
+      targetURL: url,
+      userId: req.user.id,
+    })
+    .returning({
+      id: urlsTable.id,
+      shortCode: urlsTable.shortCode,
+      targetURL: urlsTable.targetURL,
+    });
+  return res.status(201).json({
+    id: result.id,
+    shortCode: result.shortCode,
+    targetURL: result.targetURL,
+  });
+});
+
+export default router;
